cbfcf457372928b9e958fd8210fcde31d387074e
Artur Arseniev <artur.catch@hotmail.it>
2018-02-09 08:01:04 +0100

    Refactor blocks.js

--- src/blocks.js
+++ src/blocks.js
@@ -3,25 +3,45 @@ export default function (editor, opt = {}) {
   let bm = editor.BlockManager;
   let blocks = c.blocks;
   let stylePrefix = c.stylePrefix;
+  const flexGrid = c.flexGrid;
   const basicStyle = c.addBasicStyle;
-  const styleRow = `
-  .${stylePrefix}row {
-    display: table;
-    padding: 10px;
-    width: 100%;
-  }
-  @media (max-width: 768px) {
-    .${stylePrefix}cell, .${stylePrefix}cell30, .${stylePrefix}cell70 {
-      width: 100%;
-      display: block;
+  const clsRow = `${stylePrefix}row`;
+  const clsCell = `${stylePrefix}cell`;
+  const styleRow = flexGrid ? `
+    .${clsRow} {
+      display: flex;
+      justify-content: flex-start;
+      align-items: stretch;
+      flex-wrap: nowrap;
+      padding: 10px;
     }
-  }`;
-  const styleClm = `
-  .${stylePrefix}cell {
-    width: 8%;
-    display: table-cell;
-    height: 75px;
-  }`;
+    @media (max-width: 768px) {
+      .${clsRow} {
+        flex-wrap: wrap;
+      }
+    }` : `
+    .${clsRow} {
+      display: table;
+      padding: 10px;
+      width: 100%;
+    }
+    @media (max-width: 768px) {
+      .${stylePrefix}cell, .${stylePrefix}cell30, .${stylePrefix}cell70 {
+        width: 100%;
+        display: block;
+      }
+    }`;
+  const styleClm = flexGrid ? `
+    .${clsCell} {
+      min-height: 75px;
+      flex-grow: 1;
+      flex-basis: 100%;
+    }` : `
+    .${clsCell} {
+      width: 8%;
+      display: table-cell;
+      height: 75px;
+    }`;
   const styleClm30 = `
   .${stylePrefix}cell30 {
     width: 30%;
@@ -31,145 +51,134 @@ export default function (editor, opt = {}) {
     width: 70%;
   }`;
 
-  if (blocks.indexOf('column1') >= 0) {
-    bm.add('column1', {
-      label: c.labelColumn1,
-      category: 'Basic',
-      attributes: {class:'gjs-fonts gjs-f-b1'},
-      content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
-          <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-        </div>
-        ${ basicStyle ?
-          `<style>
-            ${styleRow}
-            ${styleClm}
-          </style>`
-          : ''}`
-    });
-  }
+  // Make row and column classes private
+  const privateCls = [`.${clsRow}`, `.${clsCell}`];
+  editor.on('selector:add', selector =>
+    privateCls.indexOf(selector.getFullName()) >= 0 && selector.set('private', 1))
 
-  if (blocks.indexOf('column2') >= 0) {
-    bm.add('column2', {
-      label: c.labelColumn2,
-      attributes: {class:'gjs-fonts gjs-f-b2'},
-      category: 'Basic',
-      content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
-          <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-          <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-        </div>
-        ${ basicStyle ?
-          `<style>
-            ${styleRow}
-            ${styleClm}
-          </style>`
-          : ''}`
-    });
-  }
+  const toAdd = name => blocks.indexOf(name) >= 0;
 
-  if (blocks.indexOf('column3') >= 0) {
-    bm.add('column3', {
-      label: c.labelColumn3,
-      category: 'Basic',
-      attributes: {class:'gjs-fonts gjs-f-b3'},
-      content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
-          <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-          <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-          <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-        </div>
-        ${ basicStyle ?
-          `<style>
-            ${styleRow}
-            ${styleClm}
-          </style>`
-          : ''}`
-    });
-  }
+  toAdd('column1') && bm.add('column1', {
+    label: c.labelColumn1,
+    category: 'Basic',
+    attributes: {class:'gjs-fonts gjs-f-b1'},
+    content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
+        <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+      </div>
+      ${ basicStyle ?
+        `<style>
+          ${styleRow}
+          ${styleClm}
+        </style>`
+        : ''}`
+  });
 
-  if (blocks.indexOf('column3-7') >= 0) {
-    bm.add('column3-7', {
-      label: c.labelColumn37,
-      category: 'Basic',
-      attributes: {class:'gjs-fonts gjs-f-b37'},
-      content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
-          <div class="${stylePrefix}cell ${stylePrefix}cell30" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-          <div class="${stylePrefix}cell ${stylePrefix}cell70" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
-        </div>
-        ${ basicStyle ? 
-          `<style>
-            ${styleRow}
-            ${styleClm}
-            ${styleClm30}
-            ${styleClm70}
-          </style>`
-          : ''}`
-    });
-  }
+  toAdd('column2') && bm.add('column2', {
+    label: c.labelColumn2,
+    attributes: {class:'gjs-fonts gjs-f-b2'},
+    category: 'Basic',
+    content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
+        <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+        <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+      </div>
+      ${ basicStyle ?
+        `<style>
+          ${styleRow}
+          ${styleClm}
+        </style>`
+        : ''}`
+  });
 
-  if (blocks.indexOf('text') >= 0) {
-    bm.add('text', {
-      label: c.labelText,
-      category: 'Basic',
-      attributes: {class:'gjs-fonts gjs-f-text'},
-      content: {
-        type:'text',
-        content:'Insert your text here',
-        style: {padding: '10px' },
-        activeOnRender: 1
-      },
-    });
-  }
+  toAdd('column3') && bm.add('column3', {
+    label: c.labelColumn3,
+    category: 'Basic',
+    attributes: {class:'gjs-fonts gjs-f-b3'},
+    content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
+        <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+        <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+        <div class="${stylePrefix}cell" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+      </div>
+      ${ basicStyle ?
+        `<style>
+          ${styleRow}
+          ${styleClm}
+        </style>`
+        : ''}`
+  });
 
-  if (blocks.indexOf('link') >= 0) {
-    bm.add('link', {
-      label: c.labelLink,
-      category: 'Basic',
-      attributes: {class:'fa fa-link'},
-      content: {
-        type:'link',
-        content:'Link',
-        style: {color: '#d983a6'}
-      },
-    });
-  }
+  toAdd('column3-7') && bm.add('column3-7', {
+    label: c.labelColumn37,
+    category: 'Basic',
+    attributes: {class:'gjs-fonts gjs-f-b37'},
+    content: `<div class="${stylePrefix}row" data-gjs-droppable=".${stylePrefix}cell" data-gjs-custom-name="Row">
+        <div class="${stylePrefix}cell ${stylePrefix}cell30" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+        <div class="${stylePrefix}cell ${stylePrefix}cell70" data-gjs-draggable=".${stylePrefix}row" data-gjs-custom-name="Cell"></div>
+      </div>
+      ${ basicStyle ?
+        `<style>
+          ${styleRow}
+          ${styleClm}
+          ${styleClm30}
+          ${styleClm70}
+        </style>`
+        : ''}`
+  });
 
-  if (blocks.indexOf('image') >= 0) {
-    bm.add('image', {
-      label: c.labelImage,
-      category: 'Basic',
-      attributes: {class:'gjs-fonts gjs-f-image'},
-      content: {
-        style: {color: 'black'},
-        type:'image',
-        activeOnRender: 1
-      },
-    });
-  }
+  toAdd('text') && bm.add('text', {
+    label: c.labelText,
+    category: 'Basic',
+    attributes: {class:'gjs-fonts gjs-f-text'},
+    content: {
+      type:'text',
+      content:'Insert your text here',
+      style: {padding: '10px' },
+      activeOnRender: 1
+    },
+  });
 
-  if (blocks.indexOf('video') >= 0) {
-    bm.add('video', {
-      label: c.labelVideo,
-      category: 'Basic',
-      attributes: {class:'fa fa-youtube-play'},
-      content: {
-        type: 'video',
-        src: 'img/video2.webm',
-        style: {
-          height: '350px',
-          width: '615px',
-        }
-      },
-    });
-  }
+  toAdd('link') && bm.add('link', {
+    label: c.labelLink,
+    category: 'Basic',
+    attributes: {class:'fa fa-link'},
+    content: {
+      type:'link',
+      content:'Link',
+      style: {color: '#d983a6'}
+    },
+  });
 
-  if (blocks.indexOf('map') >= 0) {
-    bm.add('map', {
-      label: c.labelMap,
-      category: 'Basic',
-      attributes: {class:'fa fa-map-o'},
-      content: {
-        type: 'map',
-        style: {height: '350px'}
-      },
-    });
-  }
+  toAdd('image') && bm.add('image', {
+    label: c.labelImage,
+    category: 'Basic',
+    attributes: {class:'gjs-fonts gjs-f-image'},
+    content: {
+      style: {color: 'black'},
+      type:'image',
+      activeOnRender: 1
+    },
+  });
+
+  toAdd('video') && bm.add('video', {
+    label: c.labelVideo,
+    category: 'Basic',
+    attributes: {class:'fa fa-youtube-play'},
+    content: {
+      type: 'video',
+      src: 'img/video2.webm',
+      style: {
+        height: '350px',
+        width: '615px',
+      }
+    },
+  });
+
+  toAdd('map') && bm.add('map', {
+    label: c.labelMap,
+    category: 'Basic',
+    attributes: {class:'fa fa-map-o'},
+    content: {
+      type: 'map',
+      style: {height: '350px'}
+    },
+  });
 }
